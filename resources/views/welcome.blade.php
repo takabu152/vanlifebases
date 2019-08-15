6c397dac99bbb69d695e6ebf086412766f27b87d
takabu152 <takaphoneg@gmail.com>
2019-08-16 03:51:11 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,12 @@
             color: #636b6f;
             font-family: 'Raleway', sans-serif;
             font-weight: 100;
-            height: 100vh;
+            /* height: 100vh; */
             margin: 0;
         }
 
         .full-height {
-            height: 100vh;
+            /* height: 100vh; */
         }
 
         .flex-center {
