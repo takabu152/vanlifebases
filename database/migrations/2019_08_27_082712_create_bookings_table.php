b75726385732866711bfc484b39632148b3b00ba
kaikana <ch10.23.01.ks@gmail.com>
2019-09-03 19:48:22 +0900

    no message

---
+++ database/migrations/2019_08_27_082712_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('guestid');
+            $table->integer('storeid');
+            $table->datetime('checkinday');
+            $table->datetime('checkoutday');
+            $table->integer('paymentmoney');
+            $table->boolean('delete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
