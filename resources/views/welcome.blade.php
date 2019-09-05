c40cf8dc8da6d9d82ba32a63c6e14e72d9676dac
daikitsuru <daiki.tsuru1108@gmail.com>
2019-09-05 22:04:44 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
 
     <script src="{{ asset('js/app.js') }}" defer></script>
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
